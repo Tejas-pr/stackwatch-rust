e4a8fd73b9d946fcc3eb9c3e99eaca3560b93358
Tejas <tejas.teju02@gmail.com>
2026-02-01 00:31:08 +0530

    basic routes woth understadning

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,21 +1,36 @@
-use poem::{get, post,handler, listener::TcpListener, web::Path, Route, Server};
+use poem::{
+    Route,
+    Server,
+    get,
+    handler,
+    listener::TcpListener,
+    post,
+    web::{ Json, Path },
+};
+
+use crate::{ request_inputs::CreateWebsiteInput, request_outputs::CreateWebsiteOutput };
+
+pub mod request_inputs;
+pub mod request_outputs;
 
 #[handler]
-fn get_website(Path(website_id): Path<String>) -> String {
-    format!("website: {}", website_id)
+fn get_website(Path(name): Path<String>) -> String {
+    format!("hello: {name}")
 }
 
 #[handler]
-fn create_website(Path(website_id): Path<String>) -> String {
-    format!("website: {}", website_id)
+fn create_website(Json(data): Json<CreateWebsiteInput>) -> Json<CreateWebsiteOutput> {
+    let response: CreateWebsiteOutput = CreateWebsiteOutput {
+        id: data.url,
+    };
+
+    Json(response)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let app = Route::new()
-    .at("/status/:website_id", get(get_website))
-    .at("/website", post(create_website));
-    Server::new(TcpListener::bind("0.0.0.0:3000"))
-      .run(app)
-      .await
+        .at("/status/:website_id", get(get_website))
+        .at("/website", post(create_website));
+    Server::new(TcpListener::bind("0.0.0.0:3000")).name("hello-world").run(app).await
 }
